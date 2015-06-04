b54a278ce8b466ecab52c352f9bc0c40eef671ad
Ioan Vranau <ioan.vranau@hp.com>
2015-06-04 18:09:02 +0300

    added first rest easy files

---
+++ iot-rest/main/java/com/platform/MessageRestService.java
@@ -0,0 +1,24 @@
+package com.platform;
+
+/**
+ * Created by vranau on 6/4/2015.
+ */
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/message")
+public class MessageRestService {
+
+    @GET
+    @Path("/{param}")
+    public Response printMessage(@PathParam("param") String msg) {
+
+        String result = "Restful example : " + msg;
+
+        return Response.status(200).entity(result).build();
+
+    }
+
+}
