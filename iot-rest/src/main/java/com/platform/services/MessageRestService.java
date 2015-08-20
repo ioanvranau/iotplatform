354fe30ac029edda0b95edf0a9e75e79b35e948f
vranau <ioan.vranau@hp.com>
2015-08-20 18:15:12 +0300

    ugly code committed

---
+++ iot-rest/src/main/java/com/platform/services/MessageRestService.java
@@ -0,0 +1,24 @@
+//package com.platform.services;
+//
+///**
+// * Created by vranau on 6/4/2015.
+// */
+//import javax.ws.rs.GET;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.PathParam;
+//import javax.ws.rs.core.Response;
+//
+////http://localhost:8089/message/1
+//@Path("/message")
+//public class MessageRestService {
+//
+//    @GET
+//    @Path("/{param}")
+//    public Response printMessage(@PathParam("param") String msg) {
+//
+//        String result = "Restful example : " + msg;
+//
+//        return Response.status(200).entity(result).build();
+//
+//    }
+//}
