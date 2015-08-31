00a5dc08bb3588a66225da8310eb06b9bc286f04
vranau <ioan.vranau@hp.com>
2015-08-31 12:07:20 +0300

    added android app client hello world

---
+++ iot-client/iot-ws-client-android/src/main/java/com/platform/iot/HelloActivity.java
@@ -0,0 +1,25 @@
+package com.platform.iot;
+
+/**
+ * Created by vranau on 8/30/2015.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelloActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_layout);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText("Hello world!");
+    }
+
+}
